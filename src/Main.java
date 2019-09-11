8a17e7877b55f8ecaffa92d72321f0aa9c26b762
Diana Tusa <diana_tusha@yahoo.com>
2019-09-11 20:51:22 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // Tipuri de date
+        String type = "golden";
+        String color = "green";
+        byte
+
+
+
+
+    }
+
+}
