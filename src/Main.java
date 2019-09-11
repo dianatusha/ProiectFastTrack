ed576c4d581340455736c1e6460904587e2db4ed
Diana Tusa <diana_tusha@yahoo.com>
2019-09-11 20:58:55 +0300

    Diana Tusa

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Diana Tusa");
+
 
         // Tipuri de date
         String type = "golden";
         String color = "green";
-        byte
+
 
 
 
