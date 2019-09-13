3a346ea4207f01ea8ea73e2a05636cc89ea314e2
Diana Tusa <diana_tusha@yahoo.com>
2019-09-13 15:10:58 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,41 @@
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Diana Tusa");
+        System.out.println("I love Java");
+
+        int num1=7;
+        int num2=8;
+        int result=num1+num2;
+        System.out.println("The addition of 7 + 8 is: " +result);
+        int num3=20;
+        int num4=5;
+        int result1=num3 / num4;
+        System.out.println("The division of 20 / 5 is: " +result1);
+        float num5=7f;
+        float num6=2f;
+        float result2=num5/num6;
+        System.out.println("The division of 7/2 is: " +result2);
+        int x=-5;
+        int y=8;
+        int z=6;
+        int result3=x+y*z;
+        System.out.println("The operation -5+8*6 has the result:" +result3);
+
+
+
+
+
+
+
 
 
         // Tipuri de date
-        String type = "golden";
-        String color = "green";
+
+
 
 
 
