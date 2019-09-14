cd3dde26e922239d56169eaebec6e33e2e7fe637
Diana Tusa <diana_tusha@yahoo.com>
2019-09-14 18:30:05 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,26 @@ public class Main {
         int z=6;
         int result3=x+y*z;
         System.out.println("The operation -5+8*6 has the result:" +result3);
+        int x1=55;
+        int x2=9;
+        int x3=9;
+        int result4=(x1+x2)%9;
+        System.out.println("The operation (55+9)%9 has the result:" +result4);
+        float y1=20f;
+        float y2=-3f;
+        float y3=5f;
+        float y4=8f;
+        float result5=y1+y2*y3/y4;
+        System.out.println("The operation 20+ -3*5/8 has the result:" +result5);
+        float z1=5f;
+        float z2=15f;
+        float z3=3f;
+        float z4=2f;
+        float z5=8f;
+        float z6=3f;
+        float result6=z1+z2/z3*z4-z5%z6;
+        System.out.println("The result of the operation 5+15/3*2-8%3 is" +result6);
+
 
 
 
