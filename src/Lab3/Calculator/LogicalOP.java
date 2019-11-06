9a4d64bf849d4fa68a82edc7de3c914d4b833e28
Diana Tusa <diana_tusha@yahoo.com>
2019-11-06 14:54:54 +0200

    commit

--- src/Lab3/Calculator/LogicalOP.java
+++ src/Lab3/Calculator/LogicalOP.java
@@ -1,18 +1,44 @@
 package Lab3.Calculator;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class LogicalOP {
         int checkBiggerNumber(int first, int second){
                 if(first>second){
                      return first;
                 }else{
-                        return second;
-                }
-
+                        return second; }
 
         }
 
-
+        void textComparison(String first, String second) {
+            if (first.equals(second)) {
+                System.out.println("Sunt la fel");
+            } else  {
+                System.out.println("Nu sunt la fel");
+            }
         }
+
+        void textAndNumber(String x, int y){
+            if (x.equals("FastTrackIT")&& y<=3)
+                System.out.println(x+" "+y);
+            else if (x!=("FastTrackIT")&& y>=4)
+                System.out.println(y+" "+x);
+        }
+
+        void snowForcast(int x){
+            if (x>8 || x==6)
+                System.out.println("The forecast snow is(cm):" + x);
+        }
+
+
+
+
+
+
+
+
+}
 
 
 
--- src/Lab3/Calculator/Main.java
+++ src/Lab3/Calculator/Main.java
@@ -3,10 +3,21 @@ package Lab3.Calculator;
 public class Main {
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        System.out.println(calc.sum(8,9));
-        System.out.println(calc.substract(8,5));
-        System.out.println(calc.multiply(98,54));
-        System.out.println(calc.division(95,5));
+        System.out.println("The result of the sum is " +calc.sum(8,9));
+        System.out.println("The result of the substraction is " +calc.substract(8,5));
+        System.out.println("The result of the multiplication is " +calc.multiply(98,54));
+        System.out.println("The result of the division is " +calc.division(95,5));
+
+        LogicalOP op=new LogicalOP();
+        System.out.println("The bigger number is: "+ op.checkBiggerNumber(5,7));
+        op.textComparison("alabalaportocala", "Fasttrack");
+        op.textAndNumber("FastTrackIT",2);
+        op.textAndNumber("Alabalaportocala",7);
+        op.snowForcast(9);
+
+        
+
+
 
     }
 
